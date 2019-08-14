15c4fe3f238615476e2b7523bc8d6a3c0ffb0422
Krishna Chaitanya Bogavalli <krishnachaitanyabogavalli@Krishnas-MacBook-Pro.local>
2019-08-14 02:18:43 -0700

    Modifying UserModel Views

--- code/resources/user.py
+++ code/resources/user.py
@@ -1,4 +1,3 @@
-import sqlite3
 from flask_restful import reqparse,Resource
 from models.user import UserModel
 
@@ -17,12 +16,7 @@ class UserRegister(Resource):
         data=UserRegister.parser.parse_args()
         if UserModel.find_by_username(data['username']):
             return {"message":"user name already Exists"},400
-        connection=sqlite3.connect('mydata.db')
-        cursor=connection.cursor()
-        query="INSERT INTO users VALUES (NULL, ?, ?)"
-        cursor.execute(query,(data['username'],data['password']))
-        connection.commit()
-        connection.close()
-
+        user=UserModel(**data)
+        user.save_to_db()
         return {"Message": "User registred Successfully"}, 201
 
